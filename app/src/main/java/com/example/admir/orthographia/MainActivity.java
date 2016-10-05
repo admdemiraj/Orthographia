c0bc4ce8d594fffc8e6e7dd4b8becc3599ce6241
cheeseplate <a_000110111>
2016-10-06 01:23:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/admir/orthographia/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.admir.orthographia;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void startVoiceActivity(View view) {
+        Intent intent = new Intent(this, VoiceListening.class);
+        startActivity(intent);
+    }
+
+}
